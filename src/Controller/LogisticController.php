c31339c22e4d8c35bc26657b3d60f7ac2792677b
TRINH <laser2u02@gmail.com>
2024-02-09 23:36:33 +0100

    all done with bonus

--- src/Controller/LogisticController.php
+++ src/Controller/LogisticController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Service\Container;
+
 class LogisticController extends AbstractController
 {
     public function index()
     {
+        $containers = new Container();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // $cupcakeNumber = trim($_POST['cupcakeNumber']);
+            $cupcakeNumber = trim($_POST['cupcakeNumber']);
             // TODO call inbox() method of Container class with $cupcakeNumber as parameter
+            // TODO affect the result of inbox() method to $inbox variable
+            $containers = $containers->inbox($cupcakeNumber);
         }
         return $this->twig->render('Logistic/index.html.twig', [
-                // TODO send a containers variable to Twig with results of inbox() method
+            // TODO send a containers variable to Twig with results of inbox() method
+            'containers' => $containers
         ]);
     }
 }
--- src/Service/Container.php
+++ src/Service/Container.php
@@ -4,4 +4,34 @@ namespace App\Service;
 
 class Container
 {
+    public const SMALL = 2;
+    public const MEDIUM = 5;
+    public const LARGE = 8;
+
+    public function inbox(int $numberCake): array
+    {
+        // Calculate the number of containers for each size
+        $numLarge = (int) ($numberCake / self::LARGE);
+        $remainingCupcakes = $numberCake % self::LARGE;
+
+        $numMedium = (int) ($remainingCupcakes / self::MEDIUM);
+        $remainingCupcakes %= self::MEDIUM;
+
+        $numSmall = (int) ceil($remainingCupcakes / self::SMALL);
+
+        // If there are more than 2 small boxes, fill medium boxes first
+        if ($numSmall >= 2) {
+            $numMedium += (int) ($numSmall / 2);
+            $numSmall %= 2;
+        }
+
+        // If there are both small and medium boxes, fill large boxes
+        if ($numSmall > 0 && $numMedium > 0) {
+            $numLarge += min($numSmall, $numMedium);
+            $numMedium -= min($numSmall, $numMedium);
+            $numSmall = 0;
+        }
+
+        return [$numLarge, $numMedium, max(0, $numSmall)];
+    }
 }
