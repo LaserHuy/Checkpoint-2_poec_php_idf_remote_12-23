2e7cabde69527f56da9b2af4bed66a26a2a0b980
TRINH <laser2u02@gmail.com>
2024-02-09 16:32:46 +0100

    list cupcake

--- src/Controller/CupcakeController.php
+++ src/Controller/CupcakeController.php
@@ -22,13 +22,33 @@ class CupcakeController extends AbstractController
     public function add()
     {
         // Initialize the cupcake manager
+        $cupcakeManager = new CupcakeManager();
+
+        $errors = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             //TODO Add your code here to create a new cupcake
-            header('Location:/cupcake/list');
+            // Validate form inputs
+            if (empty($_POST['name'])) {
+                $errors['name'] = 'Cupcake name is required.';
+            }
+            // If there are no validation errors, proceed with inserting data
+            if (empty($errors)) {
+                $cupcake = [
+                    'name' => $_POST['name'],
+                    'color1' => $_POST['color1'],
+                    'color2' => $_POST['color2'],
+                    'color3' => $_POST['color3'],
+                    'accessory_id' => $_POST['accessory'],
+                ];
+                $cupcakeManager = $cupcakeManager->insert($cupcake);
+                header('Location:/cupcake/list');
+            }
         }
         //TODO retrieve all accessories for the select options
-        return $this->twig->render('Cupcake/add.html.twig');
+        return $this->twig->render('Cupcake/add.html.twig', [
+            'errors' => $errors, 
+        ]);
     }
 
     /**
@@ -42,6 +62,12 @@ class CupcakeController extends AbstractController
     public function list()
     {
         //TODO Retrieve all cupcakes
-        return $this->twig->render('Cupcake/list.html.twig');
+        // Initialize the cupcake manager
+        $cupcakeManager = new CupcakeManager();
+        $cupcakes = $cupcakeManager->selectAll('name');
+
+        return $this->twig->render('Cupcake/list.html.twig', [
+            'cupcakes' => $cupcakes
+        ]);
     }
 }
