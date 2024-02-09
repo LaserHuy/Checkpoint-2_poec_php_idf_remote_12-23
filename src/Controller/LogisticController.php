3e35d28236c14dc667ebc05f2b1139ae07e2cf13
TRINH <laser2u02@gmail.com>
2024-02-10 00:06:33 +0100

    fix errors

--- src/Controller/LogisticController.php
+++ src/Controller/LogisticController.php
@@ -9,15 +9,23 @@ class LogisticController extends AbstractController
     public function index()
     {
         $containers = new Container();
+        $error = '';
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $cupcakeNumber = trim($_POST['cupcakeNumber']);
+            if (empty($cupcakeNumber) || !is_numeric($cupcakeNumber)) {
+                $error = 'Please enter a valid number';
+            }
             // TODO call inbox() method of Container class with $cupcakeNumber as parameter
             // TODO affect the result of inbox() method to $inbox variable
+            if (empty($error)) {
             $containers = $containers->inbox($cupcakeNumber);
+            }
         }
         return $this->twig->render('Logistic/index.html.twig', [
             // TODO send a containers variable to Twig with results of inbox() method
-            'containers' => $containers
+            'containers' => $containers,
+            'error' => $error
         ]);
     }
 }
